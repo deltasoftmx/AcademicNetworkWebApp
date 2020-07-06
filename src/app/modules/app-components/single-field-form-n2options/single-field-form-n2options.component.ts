3feb2f2339823cd60acdfe8dd6a4b3fc8b619f0d
juliocsantaman <juliocsantaman@gmail.com>
2020-07-06 06:31:32 -0500

    Se agregó una alerta personalizada con ayuda de la libreria sweetalert.

--- src/app/modules/app-components/single-field-form-n2options/single-field-form-n2options.component.ts
+++ src/app/modules/app-components/single-field-form-n2options/single-field-form-n2options.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import swal from 'sweetalert';
 
 @Component({
   selector: 'app-single-field-form-n2options',
@@ -34,8 +35,12 @@ export class SingleFieldFormN2optionsComponent implements OnInit {
     //Evitar la ejecución del flujo si el campo es
     //requerido y está vacío.
     if(!val && this.isreq && (btn == this.forward_btn || btn == 'return')) {
-      //usar algún servicio de alertas en lugar del alert del navegador.
-      alert(`El campo ${this.label} es requerido.`);
+      //Muestra una alerta de error al usuario.
+      //Descripción de los parámetros:
+      //1. Título.
+      //2. Descripción del mensaje/alerta.
+      //3. Agrega un icono (predefinidos: "warning", "error", "success" e "info").
+      swal('Error', `El campo ${this.label} es requerido.`, 'error');
       return;
     }
 
