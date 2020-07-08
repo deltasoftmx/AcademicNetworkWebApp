e72d3cc8f1f861232f178955daf32b6859c4bd96
Axel Alexis Montejo Lopez <alemontejo.lp@gmail.com>
2020-07-07 23:14:12 -0500

    Ajustes de comportamiento en los formularios. Añadido funcionalidad para cambiar el usuario cuando ya se ha pasado a escribir la contraseña.

--- src/app/modules/views/login/login.component.ts
+++ src/app/modules/views/login/login.component.ts
@@ -13,6 +13,8 @@ export class LoginComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
+    this.set_form_visibility('passwd', 'hide');
+    this.set_form_visibility('username', 'show');
   }
 
   catch_user(user_form_data) {
@@ -20,21 +22,46 @@ export class LoginComponent implements OnInit {
     //la lógica correspondiente para iniciar sesión.
     //Recordar modularizar el código (separar en funciones).
     console.log(user_form_data);
-
-    this.session.set_userdata('username', user_form_data.value);
+    if(user_form_data.btn == 'right' || user_form_data.btn == 'return') {
+      this.set_form_visibility('username', 'hide');
+      this.set_form_visibility('passwd', 'show');
+    }
   }
 
   //Muestra un formulario para que
   //el usuario pueda ingresar su contraseña
   //si el input del username no está vacío. 
   catchPassword(user_form_data) {
-    if(user_form_data.value != '') {
-      const usernameForm = document.querySelector('div.get-username');
-      const passwordForm = document.querySelector('div.get-password');
+    if(user_form_data.btn == 'left') {
+      this.set_form_visibility('passwd', 'hide');
+      this.set_form_visibility('username', 'show');
+    }
+    //Caso contrario, iniciar sesión y seguir a la vista del feed.
+    //La siguiente línea es un ejemplo, no es un inicio de sesión real.
+    this.session.set_userdata('username', user_form_data.value);
+  }
 
-      usernameForm.classList.add('inactive');
-      passwordForm.classList.remove('inactive');
-      passwordForm.classList.add('active');
+  /**
+   * Oculta o muestra los formularios.
+   * @param form:string [username|passwd]
+   * @param state:string [hide|show]
+   */
+  set_form_visibility(form, state) {
+
+    let comp;
+
+    if(form == 'username') {
+      comp = document.querySelector('div.get-username');
+    } else if(form == 'passwd') {
+      comp = document.querySelector('div.get-password');
+    }
+
+    if(state == 'hide') {
+      comp.classList.remove('active');
+      comp.classList.add('inactive');
+    } else if(state == 'show') {
+      comp.classList.remove('inactive');
+      comp.classList.add('active');
     }
   }
 
