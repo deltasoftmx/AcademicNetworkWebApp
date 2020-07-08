89804603a09f6ebd6e07101131eaf896a162dccb
juliocsantaman <juliocsantaman@gmail.com>
2020-07-07 20:40:32 -0500

    Se realizó que al pedir la contraseña del usuario se haga en la misma vista de login.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './modules/views/login/login.component';
-import { PasswordFormComponent } from './modules/views/password-form/password-form.component';
 
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'login', component: LoginComponent },
-  { path: 'password-form', component: PasswordFormComponent }
+  { path: 'login', component: LoginComponent }
 ];
 
 @NgModule({
--- src/app/modules/views/login/login.component.ts
+++ src/app/modules/views/login/login.component.ts
@@ -9,7 +9,7 @@ import { SessionService } from '../../../services/session.service';
 export class LoginComponent implements OnInit {
 
   constructor(
-    private session: SessionService
+    private session: SessionService,
   ) { }
 
   ngOnInit(): void {
@@ -19,8 +19,27 @@ export class LoginComponent implements OnInit {
     //Las siguientes líneas son un ejemplo y deben ser cambiadas por
     //la lógica correspondiente para iniciar sesión.
     //Recordar modularizar el código (separar en funciones).
-    console.log(user_form_data)
-    this.session.set_userdata('username', user_form_data.value)
+    console.log(user_form_data);
+    
+    this.showPasswordForm(user_form_data);
+
+    this.session.set_userdata('username', user_form_data.value);
+
+    
+  }
+
+  //Muestra un formulario para que
+  //el usuario pueda ingresar su contraseña
+  //si el input del username no está vacío. 
+  showPasswordForm(user_form_data) {
+    if(user_form_data.value != '') {
+      const usernameForm = document.querySelector('div.get-username');
+      const passwordForm = document.querySelector('div.get-password');
+
+      usernameForm.classList.add('inactive');
+      passwordForm.classList.remove('inactive');
+      passwordForm.classList.add('active');
+    }
   }
 
 }
