977f1762f15e52b17551a9591896a704c29e4912
juliocsantaman <juliocsantaman@gmail.com>
2020-07-10 20:42:48 -0500

    Avance del formulario de registro y realización del servicio de notificaciones.

--- src/app/modules/app-components/single-field-form-n2options/single-field-form-n2options.component.ts
+++ src/app/modules/app-components/single-field-form-n2options/single-field-form-n2options.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import swal from 'sweetalert2';
+import { NotificationsService } from '../../../services/notifications.service';
 
 @Component({
   selector: 'app-single-field-form-n2options',
@@ -19,7 +19,7 @@ export class SingleFieldFormN2optionsComponent implements OnInit {
 
   @Output('btn-pressed') btn_pressed: EventEmitter<any> = new EventEmitter();
 
-  constructor() { }
+  constructor(private notifService: NotificationsService) { }
 
   ngOnInit(): void {
   }
@@ -36,18 +36,7 @@ export class SingleFieldFormN2optionsComponent implements OnInit {
     //Evitar la ejecución del flujo si el campo es
     //requerido y está vacío.
     if(!val && this.isreq && (btn == this.forward_btn || btn == 'return')) {
-      //Muestra una alerta de error al usuario.
-      //Descripción de las propiedades del objeto como parámetro:
-      //1. icon = agrega un icono (success, error, warning, info y question)
-      //2. title = título de la alerta.
-      //3. text = descripción del mensaje/alerta.
-      //4. footer = footer de la alerta.
-      swal.fire({
-        icon: 'error',
-        title: '<strong> Error </strong>',
-        text: `El campo ${this.label} es requerido.`,
-        footer: this.footer
-      });
+      this.notifService.error('Error', `El campo ${this.label} es requerido.`, this.footer);
       return;
     }
 
--- src/app/modules/views/sign-up/sign-up.component.ts
+++ src/app/modules/views/sign-up/sign-up.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Student, Career } from '../../interfaces/student.model';
 import { SignUpService } from '../../../services/sign-up.service';
 import { Router } from '@angular/router';
-import swal from 'sweetalert2';
+import { NotificationsService } from '../../../services/notifications.service';
 
 @Component({
   selector: 'app-sign-up',
@@ -14,12 +14,15 @@ export class SignUpComponent implements OnInit {
   public student: Student;
   public careers: Career[];
 
-  constructor(private signUpService: SignUpService, private router: Router) { 
-    this.careers = signUpService.getCareers();
-    this.student = signUpService.newStudent();
-  }
+  constructor(
+    private signUpService: SignUpService, 
+    private router: Router,
+    private notifService: NotificationsService
+    ) { }
 
   ngOnInit(): void {
+    this.careers = this.signUpService.getCareers();
+    this.student = this.signUpService.newStudent();
   }
 
   //Métodos.
@@ -29,23 +32,11 @@ export class SignUpComponent implements OnInit {
     if(event.explicitOriginalTarget.textContent == 'Registrarse' || event.explicitOriginalTarget.textContent == '') {
 
       for(let propertie in this.student) {
-        
         let value = this.removeWhiteSpace(this.student[propertie]);
-
         //Si algun campo que no sea descripción está vacío
         //se le manda un mensaje de error.
         if(propertie != 'description' && value === '') {
-          //Muestra una alerta de error al usuario.
-          //Descripción de las propiedades del objeto como parámetro:
-          //1. icon = agrega un icono (success, error, warning, info y question)
-          //2. title = título de la alerta.
-          //3. text = descripción del mensaje/alerta.
-          //4. footer = footer de la alerta.
-          swal.fire({
-            icon: 'error',
-            title: '<strong> Error </strong>',
-            text: 'No puedes dejar en blanco los campos con *'
-          });
+          this.notifService.error('Error', 'No puedes dejar en blanco los campos con *');
           return;
         }
       }
@@ -55,33 +46,20 @@ export class SignUpComponent implements OnInit {
       console.log(this.signUpService.getStudents());
       this.student = this.signUpService.newStudent();
   
-      this.successMsg();
+      this.notifService.success('Correcto', 'Has sido registrado correctamente');
+      setTimeout(() => {
+        this.router.navigate(['/login']);
+      }, 1000);
 
     } else if(event.explicitOriginalTarget.textContent == 'Volver') {
       this.goBack();
     }
-
-  }
-
-
-  //Mostrando mensaje que el registro
-  //ha sido éxitoso.
-  successMsg() {
-    swal.fire({
-      icon: 'success',
-      title: '<strong> Correcto </strong>',
-      text: 'Has sido registrado correctamente'
-    });
-    setTimeout(() => {
-      this.router.navigate(['/login']);
-    }, 1000);
   }
 
   //Se quitan los espacios en blanco
-  //para validarlos.
+  //de izquierda a derecha para validarlos.
   removeWhiteSpace(str) {
-    let value = str.toString();
-    let newValue = value.replace(/ /g, '');
+    let newValue = str.toString().trim();
     return newValue;
   }
 
@@ -89,7 +67,4 @@ export class SignUpComponent implements OnInit {
    goBack() {
     this.router.navigate(['/login']);
   }
- 
-  
-
 }
