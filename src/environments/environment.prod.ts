552f4ce6c38d4920465bef11352136d618509e85
Axel Alexis Montejo Lopez <alemontejo.lp@gmail.com>
2023-01-13 00:11:30 -0500

    Changed backend domain.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,4 +3,4 @@ export const environment = {
 };
 
 export const apikey = '041e68356e063cc8cb72c3e83a8d05753e39d6c64f6eac01987003881e956e5c';
-export const domain = 'https://academic-network-backend.herokuapp.com';
+export const domain = 'http://ec2-3-17-63-120.us-east-2.compute.amazonaws.com:3001';
